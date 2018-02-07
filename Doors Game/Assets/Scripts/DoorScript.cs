7bbc4bd58517fc56dfacccf0990b6564d778d7bb
Russell <russellsng@gmail.com>
2018-02-07 12:07:34 -0500

    95% Working! Just gotta figure out the probability selection! :D

---
+++ Doors Game/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+//Enums to represent what the index in percentages list corresponds to.
+enum Doors
+{
+    Hot_Noisy_Safe = 0,
+    Hot_Noisy_NotSafe = 1,
+    Hot_NotNoisy_Safe = 2,
+    Hot_NotNoisy_NotSafe = 3,
+    NotHot_Noisy_Safe = 4,
+    NotHot_Noisy_NotSafe = 5,
+    NotHot_NotNoisy_Safe = 6,
+    NotHot_NotNoisy_NotSafe = 7,
+    Door_Selected = 8,
+};
+
+public class DoorScript : MonoBehaviour {
+
+    bool safeDoor; // Bool to track the safety of a door
+
+    //Sprite Images of the various doors
+    public Sprite safe;
+    public Sprite dead;
+
+    public Sprite hot;
+    public Sprite noisy;
+    public Sprite hotnoisy;
+    public Sprite nothot_notnoisy;
+
+    public GameObject ScriptObject;
+    private TextFileReader probabilitiesScript;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+       ScriptObject = GameObject.FindWithTag("Script Object");
+
+       probabilitiesScript = ScriptObject.GetComponent<TextFileReader>();
+
+        calculateProbabilities();
+    }
+
+    //Function to calculate which door is generated using
+    //Unity's Random function and comparing it to the percentages provided
+    public void calculateProbabilities()
+    {
+        float chance = UnityEngine.Random.value;
+        Debug.Log("Chance " + chance);
+
+        for (int i = 0; i < 8; i++)
+        {
+            float probability = probabilitiesScript.percentages[i];
+            Debug.Log("Probability " + probability);
+       
+
+            if (chance <= probability)
+            {
+                if (i == (int)Doors.Hot_Noisy_Safe)
+                {
+                    this.GetComponent<Image>().sprite = hotnoisy;
+                    safeDoor = true;
+                }
+
+                else if (i == (int)Doors.Hot_Noisy_NotSafe)
+                {
+                    this.GetComponent<Image>().sprite = hotnoisy;
+                    safeDoor = false;
+                }
+
+                else if (i == (int)Doors.Hot_NotNoisy_Safe)
+                {
+                    this.GetComponent<Image>().sprite = hot;
+                    safeDoor = true;
+                }
+
+                else if (i == (int)Doors.Hot_NotNoisy_NotSafe)
+                {
+                    this.GetComponent<Image>().sprite = hotnoisy;
+                    safeDoor = false;
+                }
+
+                else if (i == (int)Doors.NotHot_Noisy_Safe)
+                {
+                    this.GetComponent<Image>().sprite = noisy;
+                    safeDoor = true;
+                }
+
+                else if (i == (int)Doors.NotHot_Noisy_NotSafe)
+                {
+                    this.GetComponent<Image>().sprite = noisy;
+                    safeDoor = false;
+                }
+
+                else if (i == (int)Doors.NotHot_NotNoisy_Safe)
+                {
+                    this.GetComponent<Image>().sprite = nothot_notnoisy;
+                    safeDoor = true;
+                }
+
+                else if (i == (int)Doors.NotHot_NotNoisy_NotSafe)
+                {
+                    this.GetComponent<Image>().sprite = nothot_notnoisy;
+                    safeDoor = false;
+                }
+
+                else
+                {
+                    this.GetComponent<Image>().sprite = nothot_notnoisy;
+                    safeDoor = false;
+                }
+            }
+
+        }
+
+    //if (chance <= probability)
+    //     this.GetComponent<Image>().sprite = safe;
+
+    // else
+    //     this.GetComponent<Image>().sprite = dead;
+
+
+    //Debug.Log(dead);
+    // Debug.Log(gameObject.GetComponent<Image>()); 
+    //this.GetComponent<Image>().sprite = dea   d;
+    }
+
+    public void isDoorSafe()
+    {
+        if (safeDoor)
+            this.GetComponent<Image>().sprite = safe;
+
+        else if (!safeDoor)
+            this.GetComponent<Image>().sprite = dead;
+    }
+}
---
+++ Doors Game/Assets/Scripts/TextFileReader.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+//Enums to represent what the index in percentages list corresponds to.
+//enum Doors
+//{
+//    Hot_Noisy_Safe = 0,
+//    Hot_Noisy_NotSafe = 1,
+//    Hot_NotNoisy_Safe = 2,
+//    Hot_NotNoisy_NotSafe = 3,
+//    NotHot_Noisy_Safe = 4,
+//    NotHot_Noisy_NotSafe = 5,
+//    NotHot_NotNoisy_Safe = 6,
+//    NotHot_NotNoisy_NotSafe = 7,
+//    Door_Selected = 8,
+//};
+
+public class TextFileReader : MonoBehaviour
+{
+    public Text textUI; // Link to textUI element
+
+    public List<float> percentages = new List<float>(); // List of probabilities stored as floats
+
+    //bool safeDoor; // Bool to track the safety of a door
+
+    ////Sprite Images of the various doors
+    //public Sprite safe;
+    //public Sprite dead;
+
+    //public Sprite hot;
+    //public Sprite noisy;
+    //public Sprite hotnoisy;
+    //public Sprite nothot_notnoisy;
+
+
+
+    ////Function to open prompt a file dialog and output the text to a UI element
+    void Awake()
+    {
+        string path = EditorUtility.OpenFilePanel("Select Probability Text Location!", "", "txt");
+
+        if (path.Length != 0)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            textUI.text = reader.ReadToEnd();
+
+            reader = new StreamReader(path);
+
+            storeProbabilities(reader.ReadToEnd());
+            reader.Close();
+
+            //calculateProbabilities();
+        }
+    }
+
+    //Function to open a file dialog from button press and output the text to a UI element
+    public void LoadInfo()
+    {
+        string path = EditorUtility.OpenFilePanel("Select Probability Text Location!", "", "txt");
+
+        if (path.Length != 0)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            textUI.text = reader.ReadToEnd();
+
+            reader = new StreamReader(path);
+
+            storeProbabilities(reader.ReadToEnd());
+            reader.Close();
+
+            //calculateProbabilities();
+        }
+    }
+
+    //Function that parses the string obtained from the file and creates a list of the probabilities as floats.
+    public void storeProbabilities(string probs)
+    {
+        string probabilities = probs;
+
+        Debug.Log(probabilities);
+
+        string[] subStrings = probabilities.Split(new char[] { ' ', '\n' },  StringSplitOptions.RemoveEmptyEntries);
+        //string[] subStrings = probabilities.Split(' ');
+
+        for (int i = 0; i < subStrings.Length; i++)
+        {
+            float temp;
+            //Debug.Log(subStrings[i]);
+
+            bool result = float.TryParse(subStrings[i], out temp);
+
+            if (result)
+            {
+                //Debug.Log(temp);
+                percentages.Add(temp);
+            }
+
+        }
+
+        //for (int i = 0; i < percentages.Count; i++)
+        //{
+        //    Debug.Log(percentages[i]);
+        //}
+        //Debug.Log("Percentages at 4 " + percentages[4]);
+    }
+}
