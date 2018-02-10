7dd4b615a86f31589411e5bc8c7da9f59f57f913
Russell <russellsng@gmail.com>
2018-02-10 14:40:40 -0500

    Final Build! All works and resolution is fixed! High Five my dude.

--- Doors Game/Assets/Scripts/TextFileReader.cs
+++ Doors Game/Assets/Scripts/TextFileReader.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using System.IO;
 using UnityEngine.UI;
 using System;
+using SFB;
 
 //Enums to represent what the index in percentages list corresponds to.
 //enum Doors
@@ -26,35 +26,21 @@ public class TextFileReader : MonoBehaviour
 
     public List<float> percentages = new List<float>(); // List of probabilities stored as floats
 
-    //bool safeDoor; // Bool to track the safety of a door
-
-    ////Sprite Images of the various doors
-    //public Sprite safe;
-    //public Sprite dead;
-
-    //public Sprite hot;
-    //public Sprite noisy;
-    //public Sprite hotnoisy;
-    //public Sprite nothot_notnoisy;
-
-
-
-    ////Function to open prompt a file dialog and output the text to a UI element
-
     public GameObject Doors;
     private DoorScript DoorScript;
 
+    ////Function to open prompt a file dialog on program run and output the text to a UI element
     void Awake()
     {
-        string path = EditorUtility.OpenFilePanel("Select Probability Text Location!", "", "txt");
+        var path = StandaloneFileBrowser.OpenFilePanel("Select Probability Text Location!", "", "", false);
 
         if (path.Length != 0)
         {
-            StreamReader reader = new StreamReader(path);
+            StreamReader reader = new StreamReader(path[0]);
 
             textUI.text = reader.ReadToEnd();
 
-            reader = new StreamReader(path);
+            reader = new StreamReader(path[0]);
 
             storeProbabilities(reader.ReadToEnd());
             reader.Close();
@@ -64,19 +50,20 @@ public class TextFileReader : MonoBehaviour
     //Function to open a file dialog from button press and output the text to a UI element
     public void LoadInfo()
     {
-        string path = EditorUtility.OpenFilePanel("Select Probability Text Location!", "", "txt");
+        var path = StandaloneFileBrowser.OpenFilePanel("Select Probability Text Location!", "", "", false);
 
         if (path.Length != 0)
         {
-            StreamReader reader = new StreamReader(path);
+            StreamReader reader = new StreamReader(path[0]);
 
             textUI.text = reader.ReadToEnd();
 
-            reader = new StreamReader(path);
+            reader = new StreamReader(path[0]);
 
             storeProbabilities(reader.ReadToEnd());
             reader.Close();
 
+            // For loop to make each door calculate probabilities again
             for (int i = 1; i <= 20; i++)
             {
                 Doors = GameObject.Find("Door" + i);
@@ -86,35 +73,32 @@ public class TextFileReader : MonoBehaviour
         }
     }
 
-    //Function that parses the string obtained from the file and creates a list of the probabilities as floats.
+    //Function that parses the string obtained from the file and stores a list of the probabilities as floats in percentages list.
     public void storeProbabilities(string probs)
     {
+        percentages.Clear(); //Deletes any previous entries in percentages list
+
         string probabilities = probs;
 
-        Debug.Log(probabilities);
-
+        // Splits the data into substrings with ' ' and newlines as parameters to split
         string[] subStrings = probabilities.Split(new char[] { ' ', '\n' },  StringSplitOptions.RemoveEmptyEntries);
-        //string[] subStrings = probabilities.Split(' ');
 
         for (int i = 0; i < subStrings.Length; i++)
         {
             float temp;
-            //Debug.Log(subStrings[i]);
-
             bool result = float.TryParse(subStrings[i], out temp);
 
             if (result)
             {
-                //Debug.Log(temp);
                 percentages.Add(temp);
             }
 
         }
+    }
 
-        //for (int i = 0; i < percentages.Count; i++)
-        //{
-        //    Debug.Log(percentages[i]);
-        //}
-        //Debug.Log("Percentages at 4 " + percentages[4]);
+    private void Update()
+    {
+       if (Input.GetKey("escape"))
+           Application.Quit();
     }
 }
